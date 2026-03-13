d82f10e6a7af838590b6a5a7b85185d8b668b30f
qb-le <quocbaole2103@gmail.com>
2026-03-13 15:43:52 +0100

    test api created, db connection created

---
+++ Backend/src/main/java/com/stakeit/StakeitApplication.java
@@ -0,0 +1,11 @@
+package com.stakeit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StakeitApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StakeitApplication.class, args);
+    }
+}
