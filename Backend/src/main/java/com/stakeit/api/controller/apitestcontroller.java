e17427baa1c0dfb55c10e54bfa6c9fcd2edfe816
qb-le <quocbaole2103@gmail.com>
2026-03-24 13:50:31 +0100

    Test api added - dependancies fixed

--- Backend/src/main/java/com/stakeit/api/controller/apitestcontroller.java
+++ Backend/src/main/java/com/stakeit/api/controller/apitestcontroller.java
@@ -1,12 +1,15 @@
 package com.stakeit.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class apitestcontroller {
-    @GetMapping("/test")
+    @GetMapping()
     public String test() {
         return "Stakeit API works";
+
     }
 }
