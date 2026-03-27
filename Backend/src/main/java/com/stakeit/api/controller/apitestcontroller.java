fa90f0cbb03c2f6e3d632c0eb2882ef5518c2f55
qb-le <quocbaole2103@gmail.com>
2026-03-27 14:40:32 +0100

    made clickable link

--- Backend/src/main/java/com/stakeit/api/controller/apitestcontroller.java
+++ Backend/src/main/java/com/stakeit/api/controller/apitestcontroller.java
@@ -9,7 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class apitestcontroller {
     @GetMapping()
     public String test() {
-        return "Stakeit API works";
-
+        return
+            """
+            Stakeit API works
+            <a href="http://145.220.72.157:8081/actuator/health/db" target="_blank">
+                Check DB Health
+            </a>
+            """;
     }
 }
