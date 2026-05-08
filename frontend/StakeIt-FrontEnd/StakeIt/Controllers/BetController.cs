be89c0f53d7d3927f6b6c0e0c08a02444c894d35
qb-le <quocbaole2103@gmail.com>
2026-05-08 10:11:07 +0200

    unchanged items added

---
+++ frontend/StakeIt-FrontEnd/StakeIt/Controllers/BetController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StakeIt.Controllers
+{
+    public class BetController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ frontend/StakeIt-FrontEnd/StakeIt/Models/BetViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace StakeIt.Models
+{
+    public class BetViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+        public string Description { get; set; } = "";
+        public decimal Price { get; set; }
+        public string CreatedBy { get; set; } = "";
+    }
+}
