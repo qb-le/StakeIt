43bf4fcfe3e1cd8e6e963ea486da310102a321fc
qb-le <quocbaole2103@gmail.com>
2026-05-08 10:25:53 +0200

    samll test update

--- Backend/src/main/java/com/stakeit/Repo/BetRepository.java
+++ Backend/src/main/java/com/stakeit/Repo/BetRepository.java
@@ -1,7 +1,5 @@
 package com.stakeit.Repo;
 
-import com.stakeit.RequestDTO.CreateBetRequest;
-import com.stakeit.ResponseDTO.CreateBetResponse;
 import com.stakeit.entity.BetEntity;
 
 import java.util.List;
@@ -12,5 +10,4 @@ public interface BetRepository {
     List<BetEntity> readOwnBets(Integer CreatedBy);
 //    BetEntity readJoinedBets(Integer userId);
 //    String joinBet(Integer betId, Integer userId);
-
 }
